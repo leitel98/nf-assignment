7a98017d7a331efcc3214b8d210a03b0b44826b4
leitel <lucaseitel98@gmail.com>
2024-03-05 14:18:47 -0300

    fertilization graphs

--- components/Graphics.tsx
+++ components/Graphics.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { MONTHS } from '@/constants';
-import { SampleT } from '@/types';
+import { FertilizationT, SampleT } from '@/types';
 import { Chart as ChartJS, registerables } from 'chart.js';
 import { useState } from 'react';
 import { Bar } from 'react-chartjs-2';
@@ -17,20 +17,59 @@ const Graphics = ({
   const [activeYear, setActiveYear] = useState(new Date().getFullYear());
 
   const filteredSamples = samplesData.filter(
-    (data: SampleT) => data.createdAt.getFullYear() === activeYear
+    (data: SampleT) => data.createdAt?.getFullYear() === activeYear
   );
-  const samplesMonthlyAmounts = Array.from({ length: 12 }, () => 0);
-  filteredSamples.forEach((data: SampleT) => {
-    const monthIndex = new Date(data.createdAt).getMonth();
-    samplesMonthlyAmounts[monthIndex] += data.amount;
-  });
+  const samplesMonthlyAverages = filteredSamples
+    .reduce(
+      (acc: any, data: any) => {
+        const monthIndex = new Date(data.createdAt).getMonth();
+        acc[monthIndex].sum += data.amount;
+        acc[monthIndex].count += 1;
+        return acc;
+      },
+      Array.from({ length: 12 }, () => ({ sum: 0, count: 0 }))
+    )
+    .map((monthData: any) =>
+      monthData.count > 0 ? monthData.sum / monthData.count : 0
+    );
 
-  const data = {
+  const filteredFertilizations = fertilzationsData.filter(
+    (data: FertilizationT) => data.createdAt?.getFullYear() === activeYear
+  );
+
+  const fertilizationMonthlyAverages = filteredFertilizations
+    .reduce(
+      (acc: any, data: any) => {
+        const monthIndex = new Date(data.createdAt).getMonth();
+        acc[monthIndex].sum += data.amount;
+        acc[monthIndex].count += 1;
+        return acc;
+      },
+      Array.from({ length: 12 }, () => ({ sum: 0, count: 0 }))
+    )
+    .map((monthData: any) =>
+      monthData.count > 0 ? monthData.sum / monthData.count : 0
+    );
+
+  const samplesGraphData = {
     labels: MONTHS,
     datasets: [
       {
-        label: 'Samples',
-        data: samplesMonthlyAmounts,
+        label: 'Samples Carbon % Average',
+        data: samplesMonthlyAverages,
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const fertilizationGraphData = {
+    labels: MONTHS,
+    datasets: [
+      {
+        label: 'Fertilziations Kg Average',
+        data: fertilizationMonthlyAverages,
         backgroundColor: 'rgba(75, 192, 192, 0.2)',
         borderColor: 'rgba(75, 192, 192, 1)',
         borderWidth: 1,
@@ -57,10 +96,10 @@ const Graphics = ({
       </div>
       <div className='grid grid-cols-2 gap-8 w-full h-full'>
         <div className='p-4 bg-zinc-900/10 rounded-lg border border-zinc-600/60 w-full'>
-          <Bar data={data} />
+          <Bar data={samplesGraphData} />
         </div>
         <div className='p-4 bg-zinc-900/10 rounded-lg border border-zinc-600/60 w-full'>
-          <Bar data={data} />
+          <Bar data={fertilizationGraphData} />
         </div>
       </div>
     </div>
